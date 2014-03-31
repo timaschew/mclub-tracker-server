ef20d19c644fe42eeacf72a4af800a3ccfae97d1
Shawn Chain <shawn.chain@gmail.com>
2014-03-31 17:26:49 +0800

    Upgrade to grails 2.3.7

--- MClubTracker/grails-app/conf/BuildConfig.groovy
+++ MClubTracker/grails-app/conf/BuildConfig.groovy
@@ -2,15 +2,26 @@ grails.servlet.version = "2.5" // Change depending on target container complianc
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-// uncomment (and adjust settings) to fork the JVM to isolate classpaths
-//grails.project.fork = [
-//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
-//]
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -26,9 +37,8 @@ grails.project.dependency.resolution = {
 
         grailsPlugins()
         grailsHome()
+		mavenLocal()
         grailsCentral()
-
-        mavenLocal()
         mavenCentral()
 
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
@@ -45,26 +55,31 @@ grails.project.dependency.resolution = {
 		// runtime 'org.xerial:sqlite-jdbc:3.7.2'
         // runtime 'mysql:mysql-connector-java:5.1.22'
 		
-		compile 'io.netty:netty:3.6.3.Final'
-		test 'org.spockframework:spock-grails-support:0.7-groovy-2.0'
+		//compile 'io.netty:netty:3.6.3.Final'
+		compile ('org.atmosphere:nettosphere:2.0.0.RC4'){
+			excludes "slf4j-api","logback-core","logback-classic","geronimo-servlet_3.0_spec"
+		}
+//		test 'org.spockframework:spock-grails-support:0.7-groovy-2.0'
     }
 
-    plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.8.3"
-        runtime ":resources:1.1.6"
+plugins {
+        // plugins for the build system only
+        build ":tomcat:7.0.52.1"
 
+        // plugins for the compile step
+        compile ":scaffolding:2.0.0"
+        compile ':cache:1.1.1'
+
+        // plugins needed at runtime but not for compilation
+        runtime ":hibernate:3.6.10.10" // or ':hibernate4:4.3.4.1' for Hibernate 4
+        runtime ":database-migration:1.3.5"
+        runtime ":jquery:1.10.2"
+        runtime ":resources:1.2.7"
         // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
+        //runtime ":zipped-resources:1.0.1"
+        //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
-
-        build ":tomcat:$grailsVersion"
-
-        runtime ":database-migration:1.3.2"
-
-        compile ':cache:1.0.1'
-		
 //		compile ":gwt:0.8"
+		compile ":quartz:1.0-RC10"
     }
 }
