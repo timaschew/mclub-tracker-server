07f2e11061b73fa9e6ad8c53e5ac7260a8cc9588
Shawn Chain <shawn.chain@gmail.com>
2016-07-14 15:23:53 +0800

    Update trackerMapController

--- MClubTracker/grails-app/controllers/mclub/tracker/TrackerMapController.groovy
+++ MClubTracker/grails-app/controllers/mclub/tracker/TrackerMapController.groovy
@@ -1,10 +1,9 @@
 package mclub.tracker
 
-
+import mclub.user.AuthUtils
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
-import mclub.user.AuthUtils
 
 @Transactional(readOnly = true)
 class TrackerMapController {
@@ -13,85 +12,90 @@ class TrackerMapController {
 
     def index(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        respond TrackerMap.list(params), model:[trackerMapInstanceCount: TrackerMap.count()]
+        respond TrackerMap.list(params), model:[trackerMapCount: TrackerMap.count()]
     }
 
-    def show(TrackerMap trackerMapInstance) {
-        respond trackerMapInstance
+    def show(TrackerMap trackerMap) {
+        respond trackerMap
     }
 
     def create() {
-		TrackerMap map = new TrackerMap(params);
-		if(map.uniqueId == null){
-			//generatr unique id for the new map
-			map.uniqueId = AuthUtils.generateSalt(7).toLowerCase();
-		}
+        TrackerMap map = new TrackerMap(params);
+        if(map.uniqueId == null){
+            //generatr unique id for the new map
+            map.uniqueId = AuthUtils.generateSalt(7).toLowerCase();
+        }
         respond map
     }
 
     @Transactional
-    def save(TrackerMap trackerMapInstance) {
-        if (trackerMapInstance == null) {
+    def save(TrackerMap trackerMap) {
+        if (trackerMap == null) {
+            transactionStatus.setRollbackOnly()
             notFound()
             return
         }
 
-        if (trackerMapInstance.hasErrors()) {
-            respond trackerMapInstance.errors, view:'create'
+        if (trackerMap.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond trackerMap.errors, view:'create'
             return
         }
 
-        trackerMapInstance.save flush:true
+        trackerMap.save flush:true
 
         request.withFormat {
             form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'trackerMap.label', default: 'TrackerMap'), trackerMapInstance.id])
-                redirect trackerMapInstance
+                flash.message = message(code: 'default.created.message', args: [message(code: 'trackerMap.label', default: 'TrackerMap'), trackerMap.id])
+                redirect trackerMap
             }
-            '*' { respond trackerMapInstance, [status: CREATED] }
+            '*' { respond trackerMap, [status: CREATED] }
         }
     }
 
-    def edit(TrackerMap trackerMapInstance) {
-        respond trackerMapInstance
+    def edit(TrackerMap trackerMap) {
+        respond trackerMap
     }
 
     @Transactional
-    def update(TrackerMap trackerMapInstance) {
-        if (trackerMapInstance == null) {
+    def update(TrackerMap trackerMap) {
+        if (trackerMap == null) {
+            transactionStatus.setRollbackOnly()
             notFound()
             return
         }
 
-        if (trackerMapInstance.hasErrors()) {
-            respond trackerMapInstance.errors, view:'edit'
+        if (trackerMap.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond trackerMap.errors, view:'edit'
             return
         }
 
-        trackerMapInstance.save flush:true
+        trackerMap.save flush:true
 
         request.withFormat {
             form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'TrackerMap.label', default: 'TrackerMap'), trackerMapInstance.id])
-                redirect trackerMapInstance
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'trackerMap.label', default: 'TrackerMap'), trackerMap.id])
+                redirect trackerMap
             }
-            '*'{ respond trackerMapInstance, [status: OK] }
+            '*'{ respond trackerMap, [status: OK] }
         }
     }
 
     @Transactional
-    def delete(TrackerMap trackerMapInstance) {
+    def delete(TrackerMap trackerMap) {
 
-        if (trackerMapInstance == null) {
+        if (trackerMap == null) {
+            transactionStatus.setRollbackOnly()
             notFound()
             return
         }
 
-        trackerMapInstance.delete flush:true
+        trackerMap.delete flush:true
 
         request.withFormat {
             form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'TrackerMap.label', default: 'TrackerMap'), trackerMapInstance.id])
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'trackerMap.label', default: 'TrackerMap'), trackerMap.id])
                 redirect action:"index", method:"GET"
             }
             '*'{ render status: NO_CONTENT }
